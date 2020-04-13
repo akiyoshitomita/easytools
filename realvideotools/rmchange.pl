b82034b7000291a59ed8e30c78d2917477ebc445
akiyoshitomita <tomita@tomita.com>
2020-04-13 11:18:02 +0900

    rmviewの仕様変更
    出力形式をCSVからJSONへ変更
    logical-fileinfoヘッダに対応するため

--- realvideotools/rmchange.pl
+++ realvideotools/rmchange.pl
@@ -1,33 +1,55 @@
 #!/usr/bin/perl
 
+use JSON;
+
 my @filelist = (map{ $_ =~ tr/\n\r//d; $_ } `find . -name '*.rm'`,
 	    map{ $_ =~ tr/\n\r//d; $_ } `find . -name '*.ram'`);
 
-foreach my $file (@filelist){
-  my ($fname, $status, $title, $auther, $copyright, $comment, $message)
-       = split(/,/, `./rmview ${file} | tee -a rmview.log`);
+foreach my $file_rm (@filelist){
+  my $file_mp4   = $file_rm;
+  my $file_rm_e  = $file_rm;
+  $file_rm_e     =~ s/([() ])/\\$1/g; # エスケープ文字挿入
+  my $file_mp4_e = $file_rm_e;
+  $file_mp4   =~ s/\.rm$/\.mp4/;
+  $file_mp4   =~ s/\.ram$/\.mp4/;
+  $file_mp4_e =~ s/\.rm$/\.mp4/;
+  $file_mp4_e =~ s/\.ram$/\.mp4/;
+  next if(-f $file_mp4);
+
+  # SJISのタイトル取得
+  my $idata 
+       = `./rmview ${file_rm_e} | tee -a rmview.log`;
+  my $data = decode_json ($idata);
   $message =~ s/\n$//;
-  if($status ne 'success'){
+  # 取得エラーがある場合は終了
+  if($data->{state} ne 'success'){
     printf("%s : ERROR (rmview)\n", $file);
     next;
   }
-  my $out = `mplayer -dumpstream ${fname} -really-quiet`;
-  $file =~ s/\.rm$/\.mp4/;
-  $file =~ s/\.ram$/\.mp4/;
-  $out .= `ffmpeg -i stream.dump ${file} -loglevel quiet`;
+
+  # mplayerでストリームダンプ
+  my $out = `mplayer -dumpstream ${file_rm_e} -really-quiet`;
+  # ストリームダンプからmp4を作成
+  $out .= `ffmpeg -i stream.dump ${file_mp4_e} -loglevel quiet`;
+  # 不要なストリームダンプを削除
   `rm stream.dump`;
-  if($message eq 'encode SJIS to UTF8' ||
-     $message eq 'change copyright symble'){
+  # タイトル部分がSJISやcopyrightマークがある場合は以下の処理を行う
+  if($data->{msg} eq 'encode SJIS to UTF8' ||
+     $data->{msg} eq 'change copyright symble'){
+    # タイトル、コメント、著作権の上書き
+    my $d = $data->{data};
     my $opt = "";
-    $opt .= " --title '${title}'" if ${title};
-    $opt .= " --comment '${comment}'" if ${comment};
-    $opt .= " --copyright '${copyright}'" if ${copyright};
-    $out .= `AtomicParsley ${file}${opt}` if $opt; 
-    `rm ${file}`;
-    my $f2 = $file;
-    $f2 =~ s/\.mp4$/-temp-*.mp4/;
-    `mv ${f2} ${file}`;
+    $opt .= " --title '$d->{title}'" if $d->{title};
+    $opt .= " --comment '$d->{comment}'" if $d->{comment};
+    $opt .= " --copyright '$d->{copyright}'" if $d->{copyright};
+    $out .= `AtomicParsley ${file_mp4_e} ${opt}` if $opt; 
+    # xxx-temp-xxxx.mp4 ファイルができるので古いファイルの削除して名前変更
+    `rm ${file_mp4_e}`;
+    my $file_mp4_temp = $file_mp4_e;
+    $file_mp4_temp    =~ s/\.mp4$/-temp-*.mp4/;
+    `mv ${file_mp4_temp} ${file_mp4_e}`;
   }
   printf("%s : converted to mp4 \n", $file);
+  printf($out);
 
 }
--- realvideotools/rmview.c
+++ realvideotools/rmview.c
@@ -24,10 +24,24 @@
 #include <iconv.h>
 
 #define BUFSIZE 1024
-#define ERR001 "%s,error,,,,,%s\n"
+#define ERR001  "{\"file\":\"%s\",\"state\":\"error\",\"msg\":\"%s\"}\n"
+#define SUCCESS "{\"file\":\"%s\",\"state\":\"success\",\"data\":{%s},\"msg\":\"%s\"}\n"
+
+// SJIS変換した場合にON
+#define FLG_SJIS 0x00000001 
+// (c)マークを変換した場合にON
+#define FLG_COPY 0x00000002
+// 無変換で出力した場合
+#define FLG_NONE 0x00000004
+// 変換元データが0x00でパディングされていた場合
+// ただし 0x00以降にパディング文字以外が存在した場合はエラーとする
+#define FLG_0PAD 0x02000000
+// 変換データが正しくなかった場合
+#define FLG_UNKW 0x01000000
 
 void view_help();
 int existFile();
+int AutoEnc();
 
 #pragma pack (1)
 typedef struct {
@@ -48,34 +62,58 @@ typedef struct {
 
 int main(int argc, char *argv[]){
 
-  RmfHeader   rmfh;
-  pHeader     phead;
-  int         i, tmp, len, c_tl, c_al, c_crl, c_cml, c_len;
-  int         flgSjis, flgCopy, flgUnkw, flgCont;
-  short       ver;
-  char        *buf, *buf8;
-  char        *p1, *p2, *plen;
-  size_t      p1len, p2len;
-  char        *c_t, *c_a, *c_cr , *c_cm, *ptmp;
-  iconv_t     ic;
+  RmfHeader   rmfh;	// RMF file メインヘッダ
+  pHeader     phead;	// プロパティヘッダ
+  iconv_t     ic;	// iconv用変換コード (SJIS => UTF-8固定)
+  int         flg;      // フラグ保存場所
+  int         flgCont;  // COSTプロパティの重複チェック用
+  // CONT フィールド処理用
+  int         c_tl, c_al, c_crl, c_cml;	// タイトル,作成者,著作権,コメントの長さ
+  char        *c_t, *c_a, *c_cr, *c_cm; // 開始位置のポインタ(ibuf内部)
+  short       ver;	// バージョンの値の一時保管
+  
+  char        *ibuf;    // 入力側バッファ先頭ポインタ
+  char        *obuf;    // 出力側バッファ先頭ポインタ
+  char        *p1;	// 入力側可変ポインタ
+  char        *p2;      // 出力側可変ポインタ
+  char        *p3;      // 一時利用可変ポインタ
+  size_t      p1len;    // 入力バッファサイズ
+  size_t      p2len;    // 出力バッファの残りサイズ
 
-  buf  = (char *)malloc(BUFSIZE);
-  buf8 = (char *)malloc(BUFSIZE);
+  int         i, tmp, len;	// 一時利用
+
+
+  // 廃止予定
+  //int         flgSjis, flgCopy, flgUnkw, flgCont; // 各フラグ
+  char        *ptmp;
+
+  ibuf  = (char *)malloc(BUFSIZE);
+  obuf = (char *)malloc(BUFSIZE);
   ic   = iconv_open("UTF-8", "Shift-JIS");
+
+  p2    = obuf;
+  p2len = BUFSIZE;
+  flg   = 0;
+  flgCont = 0;
+
+  // 文字コードのパターン確認 （削除予定) 
+//  flgSjis = 0;
+//  flgCopy = 0;
+//  flgUnkw = 0;
 
   // オプションを入力していない場合はエラーとする
   if(argc != 2){ 
     view_help();
     return 0;
   }
-  // 入力ファイルが存在しない場合のエラー
-  if(!existFile(argv[1])){
-    printf(ERR001, argv[1] , "File Not Found");
-    return 4;
-  }
 
   // 入力ファイル読み取り
   FILE *fr = fopen(argv[1], "r");
+  // 入力ファイルが存在しない場合のエラー
+  if(fr == 0){
+    printf(ERR001, argv[1] , "File Not Found");
+    return 4;
+  }
   
   // RMF ヘッダを受信
   tmp = fread(&rmfh, sizeof(rmfh), 1, fr);  
@@ -89,206 +127,57 @@ int main(int argc, char *argv[]){
     return 12;  
   }
 
-  flgCont = 0;
   while(fread(&phead, sizeof(phead), 1, fr) == 1){
     if(memcmp(phead.type, "CONT", 4) == 0){
+      // CONTプロパティが2つあった場合エラーとする
       if(flgCont){
         fprintf(stderr, "Duplicate Cont area\n");
         return 12; 
       }
       flgCont = 1;
+
+      // CONT プロパティをメモリに読み込む
       if(fseek(fr, 0- sizeof(phead), SEEK_CUR)) { return 16; } 
       len = bswap_32(phead.size);  
-      if(fread(buf, len, 1, fr) != 1){   
+      if(fread(ibuf, len, 1, fr) != 1){   
         fprintf(stderr, "READ FILE ERROR\n");
         return 12; 
       }
-      p1 = buf;
-      // ヘッダのコピー
-      ptmp = buf + sizeof(phead);
-      //c_tl = bswap_16( ptmp* );
-      ver = bswap_16(*(short *)ptmp);
-      ptmp += sizeof(short);
+
+      p1  = ibuf; // 可変ポインタ 
+      // プロパティヘッダ分の移動
+      p1 += sizeof(phead);
+      // バージョンの判断
+      ver = bswap_16(*(short *)p1);
+      p1 += sizeof(short);
       if( ver != 0){
         fprintf(stderr, "ERROR: CONT VERSION\n");
 	return 20;
       } 
-      c_tl = bswap_16(*(short *)ptmp); 
-      ptmp += sizeof(short);
-      c_t  = ptmp; 
-      ptmp += c_tl;
-      c_al = bswap_16(*(short *)ptmp); 
-      ptmp += sizeof(short);
-      c_a  = ptmp; 
-      ptmp += c_al;
-      c_crl = bswap_16(*(short *)ptmp); 
-      ptmp += sizeof(short);
-      c_cr  = ptmp; 
-      ptmp += c_crl;
-      c_cml = bswap_16(*(short *)ptmp); 
-      ptmp += sizeof(short);
-      c_cm  = ptmp; 
-      ptmp += c_cml;
-
-      if( len != ptmp - buf){
+      // タイトル部分の処理 
+      for(i = 0; i < 4; i++){ 
+        p1len = bswap_16(*(short *)p1); 	// タイトル部分長さ取得
+        p1 += sizeof(short);	
+        if(p1len){			// タイトル部分がある場合
+          p3 = p1;			// タイトルテキスト部ポインタ取得	
+        	p1 += p1len;			// ポインタを次の位置へ移動
+  	if(i == 0) tmp = sprintf(p2, "\"title\":\"");
+  	if(i == 1) tmp = sprintf(p2, "\"author\":\"");
+  	if(i == 2) tmp = sprintf(p2, "\"copyright\":\"");
+  	if(i == 3) tmp = sprintf(p2, "\"comment\":\"");
+  	p2 += tmp; p2len -= tmp;
+          flg |= AutoEnc(ic,&p3, &p1len, &p2, &p2len);
+  	*p2 = '"'; p2++; p2len--; 	// 	
+  	*p2 = ','; p2++; p2len--; 	// カンマの挿入	
+        } 
+      }
+      if( len != p1 - ibuf){
         fprintf(stderr, "ERROR: CONT FORMAT\n");
 	return 20;
       } 
      
-      // 文字コードのパターン確認 
-      flgSjis = 0;
-      flgCopy = 0;
-      flgUnkw = 0;
-
-      ptmp = c_t; 
-      for(i = 0; i < c_tl; i++){
-	if(*ptmp < 0x20)      { flgUnkw = 1; }
-	else if(*ptmp < 0x7f) {}
-	else if(*ptmp < 0x9f) { flgSjis = 1; break; }
-	else if(*ptmp == 0xa9){ flgCopy = 1; }
-	else if(*ptmp < 0xdf) { flgUnkw = 1; }
-	else if(*ptmp < 0xef) { flgSjis = 1; break; }
-	else                  { flgUnkw = 1; }
-	ptmp++;
-      }
-
-      ptmp = c_a; 
-      for(i = 0; i < c_al; i++){
-	if(*ptmp < 0x20)      { flgUnkw = 1; }
-	else if(*ptmp < 0x7f) {}
-	else if(*ptmp < 0x9f) { flgSjis = 1; break; }
-	else if(*ptmp == 0xa9){ flgCopy = 1; }
-	else if(*ptmp < 0xdf) { flgUnkw = 1; }
-	else if(*ptmp < 0xef) { flgSjis = 1; break; }
-	else                  { flgUnkw = 1; }
-	ptmp++;
-      }
-
-      ptmp = c_cr; 
-      for(i = 0; i < c_crl; i++){
-	if(*ptmp < 0x20)      { flgUnkw = 1; }
-	else if(*ptmp < 0x7f) {}
-	else if(*ptmp < 0x9f) { flgSjis = 1; break; }
-	else if(*ptmp == 0xa9){ flgCopy = 1; }
-	else if(*ptmp < 0xdf) { flgUnkw = 1; }
-	else if(*ptmp < 0xef) { flgSjis = 1; break; }
-	else                  { flgUnkw = 1; }
-	ptmp++;
-      }
-
-      ptmp = c_cm; 
-      for(i = 0; i < c_cml; i++){
-	if(*ptmp == 0x00)     {}      
-	else if(*ptmp < 0x20) { flgUnkw = 1; }
-	else if(*ptmp < 0x7f) {}
-	else if(*ptmp < 0x9f) { flgSjis = 1; break; }
-	else if(*ptmp == 0xa9){ flgCopy = 1; }
-	else if(*ptmp < 0xdf) { flgUnkw = 1; }
-	else if(*ptmp < 0xef) { flgSjis = 1; break; }
-	else                  { flgUnkw = 1; }
-	ptmp++;
-      }
-
-      if(flgUnkw) {
-        fprintf(stderr, "UNKNOWN ENCODE\n");
-	return 24;
-      }
-
-      // 出力を作成
-      p2 = buf8;
-      p2len = BUFSIZE;
-     
-      tmp = snprintf(p2, p2len, "%s,success,", argv[1]);
-      p2 += tmp; p2len -= tmp;
-      
-      // SJISの変換
-      if(flgSjis){
-	// title部分
-        p1 = c_t;
-        tmp = c_tl; 
-        if( iconv(ic, &p1, &tmp, &p2, &p2len) < 0){
-          printf(ERR001, argv[1] , "SJIS ENCODE ERROR");
-	  return 28;
-        }
-        tmp = snprintf(p2, p2len, ",");
-        p2 += tmp; p2len -= tmp;
-
-	// auther部分
-        p1 = c_a;
-        tmp = c_al; 
-        if( iconv(ic, &p1, &tmp, &p2, &p2len) < 0){
-          printf(ERR001, argv[1] , "SJIS ENCODE ERROR");
-	  return 28;
-        }
-        tmp = snprintf(p2, p2len, ",");
-        p2 += tmp; p2len -= tmp;
-
-	// copyright部分
-        p1 = c_cr;
-        tmp = c_crl; 
-        if( iconv(ic, &p1, &tmp, &p2, &p2len) < 0){
-          printf(ERR001, argv[1] , "SJIS ENCODE ERROR");
-	  return 28;
-        }
-        tmp = snprintf(p2, p2len, ",");
-        p2 += tmp; p2len -= tmp;
-
-	// comment部分
-        p1 = c_cm;
-        tmp = c_cml; 
-        if( iconv(ic, &p1, &tmp, &p2, &p2len) < 0){
-          printf(ERR001, argv[1] , "SJIS ENCODE ERROR");
-	  return 28;
-        }
-        tmp = snprintf(p2, p2len, ",encode SJIS to UTF8");
-        p2 += tmp; p2len -= tmp;
-
-
-      } else if( flgCopy){
-	// title 
-        p1 = c_t;	
-	for(i=0; i<c_tl;i++){
-          if(*p1 == 0xa9){ *p2 = 0xc2; p2++; p2len--; }
-	  *p2 = *p1; p2++; p2len--; p1++;}
-        tmp = snprintf(p2, p2len, ",");
-        p2 += tmp; p2len -= tmp;
-        p1 = c_a;	
-	for(i=0; i<c_al;i++){
-          if(*p1 == 0xa9){ *p2 = 0xc2; p2++; p2len--; }
-	  *p2 = *p1; p2++; p2len--; p1++;}
-        tmp = snprintf(p2, p2len, ",");
-        p2 += tmp; p2len -= tmp;
-        p1 = c_cr;	
-	for(i=0; i<c_crl;i++){
-          if(*p1 == 0xa9){ *p2 = 0xc2; p2++; p2len--; }
-	  *p2 = *p1; p2++; p2len--; p1++;}
-        tmp = snprintf(p2, p2len, ",");
-        p2 += tmp; p2len -= tmp;
-        p1 = c_cm;	
-	for(i=0; i<c_cml;i++){
-          if(*p1 == 0xa9){ *p2 = 0xc2; p2++; p2len--; }
-	  *p2 = *p1; p2++; p2len--; p1++;}
-        tmp = snprintf(p2, p2len, ",change copyright symble");
-        p2 += tmp; p2len -= tmp;
-
-      } else if( flgUnkw){
-        fprintf(stderr, "UNKNOWN CHARSET\n");
-	return 32;
-
-      } else {
-	memcpy(p2,c_t,c_tl); p2 += c_tl; *p2 = ','; p2++;
-	memcpy(p2,c_a,c_al); p2 += c_al; *p2 = ','; p2++;
-	memcpy(p2,c_cr,c_crl); p2 += c_crl; *p2 = ','; p2++;
-	if(*p2 ==0){
-	  *p2 = ','; p2++;
-	} else {
-	  memcpy(p2,c_cm,c_cml); p2 += c_cml; *p2 = ','; p2++;
-	}
-        tmp = snprintf(p2, p2len, ",not change");
-        p2 += tmp; p2len -= tmp;
-	
-      } 
-      //tmp = bswap_32(*(int *)(buf8+4));
+    //} else  {  // 次のエリアまでコピー
+    // MDPRで logical-fileinfoの場合の処理
     } else {  // 次のエリアまでコピー
       // ファイルポインタを戻す
       if(fseek(fr, 0- sizeof(phead), SEEK_CUR)) { return 12; }
@@ -296,13 +185,13 @@ int main(int argc, char *argv[]){
       while(tmp > 0)
       {
         if(tmp > BUFSIZE){
-          if(fread(buf, BUFSIZE,1, fr) != 1){
+          if(fread(ibuf, BUFSIZE,1, fr) != 1){
             printf(ERR001, argv[1] , "FILE READ ERROR");
             return 12;
           }
           tmp -= BUFSIZE;
         } else {
-          if(fread(buf, tmp,1, fr) != 1){
+          if(fread(ibuf, tmp,1, fr) != 1){
             printf(ERR001, argv[1] , "FILE READ ERROR");
             return 12;
           }
@@ -314,25 +203,64 @@ int main(int argc, char *argv[]){
 
   }  
   fclose(fr);
-  if(flgCont){
-    printf("%s\n", buf8);
-  }
-  else {
-    printf(ERR001, argv[1] , "NO CONT AREA");
-    return 4;
+ 
+  if(flg&FLG_UNKW){
+    printf(ERR001, argv[1] , "ENCODE ERROR");
+  } 
+  else{
+    if(p2 != obuf){ p2--; *p2 = 0;} 
+    if(flg & FLG_SJIS) 
+      printf(SUCCESS, argv[1], obuf, "encode SJIS to UTF8"); 
+    else if( flg & FLG_COPY)
+      printf(SUCCESS, argv[1], obuf, "change copyright symble"); 
+    else printf(SUCCESS, argv[1], obuf, "NONE CHANGE");
   }
 
   return 0;
 }
 
-// ファイルの存在確認
-int existFile(const char* path){
-  FILE* fp = fopen(path, "r");
-  if (fp == NULL){
-    return 0;
+// 自動エンコード
+int AutoEnc(iconv_t cd, char **inbuf, int *inlen, char **outbuf, int *outlen)
+{
+  int res = 0;
+  char *ibuf = *inbuf;
+  char *obuf = *outbuf;
+  int i;
+  char *p1;
+
+  // 文字コードの判断 * SJISの場合はiconvに任せる
+  p1 = ibuf;
+  for(i = 0; i < *inlen; i++){
+    if      (*p1 == 0x00)    { res |= FLG_0PAD; } // NULLパディング
+    else if (res & FLG_0PAD) { res |= FLG_UNKW; } // NULLパディングエラー
+    else if (*p1 <  0x20)    { res |= FLG_UNKW; } // 制御文字エラー    
+    else if (*p1 <  0x7f)    {}                   // ASCII文字
+    else if (*p1 <= 0x9f)    { res |= FLG_SJIS; break; } // SJIS 
+    else if (*p1 == 0xa9)    { res |= FLG_COPY; } // (c)マーク SJIS以外
+    else if (*p1 <= 0xdf)    { res |= FLG_UNKW; } // 半角カナ エラーとする
+    else if (*p1 <= 0xef)    { res |= FLG_SJIS; break; } // SJIS
+    else                     { res |= FLG_UNKW; }
+    p1++;
   }
-  fclose(fp);
-  return 1;
+
+  // 変換不可能な場合は何もしない
+  if(res&FLG_UNKW){ return res; }
+  // SJISの場合
+  if(res&FLG_SJIS){
+    i = iconv(cd, inbuf, inlen, outbuf, outlen);
+    if(i < 0) { res != FLG_UNKW; }
+    return res;
+  }
+  if(res&FLG_COPY || 0 == res || FLG_0PAD == res){
+    while(*inlen){
+      if(**inbuf == 0x00) break; 
+      if(**inbuf == 0xa9){ **outbuf = 0xc2; *outbuf = *outbuf + 1; *outlen--; }
+      **outbuf = **inbuf; *outbuf=*outbuf+1;*inbuf=*inbuf+1;*inlen--; *outlen--;
+    }
+    return res;
+  }
+
+  return res;
 }
 
 // ヘルプ表示
